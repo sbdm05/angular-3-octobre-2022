c830ac58825817fb33ad3f2efcecb8e4f27fa881
Sonia-Baibou <s.baibou@ohmycode.io>
2022-10-03 15:45:36 +0200

    8 - ng-class/event-binding

--- src/app/ui/components/ui/ui.component.ts
+++ src/app/ui/components/ui/ui.component.ts
@@ -3,13 +3,22 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-ui',
   templateUrl: './ui.component.html',
-  styleUrls: ['./ui.component.scss']
+  styleUrls: ['./ui.component.scss'],
 })
 export class UiComponent implements OnInit {
 
-  constructor() { }
+  // créer une propriété booléenne open
+  public open: boolean = true;
 
-  ngOnInit(): void {
+  constructor() {}
+
+  ngOnInit(): void {}
+
+  public toggle() {
+    // console.log('cliqué');
+    // faire alterner cette propriété open
+    this.open = !this.open;
+    console.log(this.open);
+
   }
-
 }
