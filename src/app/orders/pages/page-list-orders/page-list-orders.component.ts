a60c830e895d475528911ec782c57439c86e6225
Sonia-Baibou <s.baibou@ohmycode.io>
2022-10-04 15:49:09 +0200

    Observable + appel HTTP

--- src/app/orders/pages/page-list-orders/page-list-orders.component.ts
+++ src/app/orders/pages/page-list-orders/page-list-orders.component.ts
@@ -1,15 +1,23 @@
 import { Component, OnInit } from '@angular/core';
+import { OrdersService } from '../../services/orders.service';
 
 @Component({
   selector: 'app-page-list-orders',
   templateUrl: './page-list-orders.component.html',
-  styleUrls: ['./page-list-orders.component.scss']
+  styleUrls: ['./page-list-orders.component.scss'],
 })
 export class PageListOrdersComponent implements OnInit {
+  public collection!: any;
 
-  constructor() { }
-
-  ngOnInit(): void {
+  // obtenir le tableau
+  constructor(private ordersService: OrdersService) {
+    // accéder à la propriété collection
+    this.ordersService.collection.subscribe((data) => {
+      // console.log(data);
+      this.collection = data;
+      console.log(this.collection);
+    });
   }
 
+  ngOnInit(): void {}
 }
---
+++ src/app/orders/services/orders.service.ts
@@ -0,0 +1,30 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { Order } from 'src/app/core/models/order';
+
+// décorateur
+@Injectable({
+  providedIn: 'root',
+})
+export class OrdersService {
+  private collection$!: Observable<Order[]>;
+
+  constructor(private http: HttpClient) {
+    // this.http.get('urlAPI');
+    this.collection = this.http.get<Order[]>('http://localhost:4009/orders');
+  }
+
+  // créer des prorpiétés et méthodes
+
+  // appel http
+  get collection() {
+    return this.collection$;
+  }
+  // this.ordersService.collection.subscribe()
+
+  // setter
+  set collection(col: Observable<Order[]>) {
+    this.collection$ = col;
+  }
+}
