339bf050242d654cf65985a0e69d94f2a6c7cdd3
Sonia-Baibou <s.baibou@ohmycode.io>
2022-10-04 09:25:57 +0200

    13 - router intermédiaire

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,24 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { PageForgotPasswordComponent } from './login/pages/page-forgot-password/page-forgot-password.component';
+import { PageResetPasswordComponent } from './login/pages/page-reset-password/page-reset-password.component';
+import { PageSignInComponent } from './login/pages/page-sign-in/page-sign-in.component';
+import { PageSignUpComponent } from './login/pages/page-sign-up/page-sign-up.component';
+import { PageNotFoundComponent } from './page-not-found/pages/page-not-found/page-not-found.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: '/sign-in', pathMatch: 'full' },
+  { path: 'sign-in', component: PageSignInComponent },
+  // créer route pour sign-up; reset-password et forgot-password
+  { path: 'sign-up', component: PageSignUpComponent },
+  { path: 'reset-password', component: PageResetPasswordComponent },
+  { path: 'forgot-password', component: PageForgotPasswordComponent },
+  // cas 404 = wildcard
+  { path: '**', component: PageNotFoundComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
--- src/app/ui/ui.module.ts
+++ src/app/ui/ui.module.ts
@@ -2,18 +2,21 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { UiComponent } from './components/ui/ui.component';
 import { CoreModule } from '../core/core.module';
+import { Ui2Component } from './components/ui2/ui2.component';
 
 
 
 @NgModule({
   declarations: [
-    UiComponent
+    UiComponent,
+    Ui2Component
   ],
   imports: [
     CommonModule
   ],
   exports: [
-    UiComponent
+    UiComponent,
+    Ui2Component
   ]
 })
 export class UiModule { }
