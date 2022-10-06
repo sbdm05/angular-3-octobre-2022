f84055266479ef43b364b3f0265ab89a8fa99351
Sonia-Baibou <s.baibou@ohmycode.io>
2022-10-06 11:16:40 +0200

    btn

--- src/app/shared/components/btn/btn.component.ts
+++ src/app/shared/components/btn/btn.component.ts
@@ -1,15 +1,16 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-btn',
   templateUrl: './btn.component.html',
-  styleUrls: ['./btn.component.scss']
+  styleUrls: ['./btn.component.scss'],
 })
 export class BtnComponent implements OnInit {
+  @Input() label!: string;
+  @Input() route!: string;
+  @Input() styleBtn!: string;
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
